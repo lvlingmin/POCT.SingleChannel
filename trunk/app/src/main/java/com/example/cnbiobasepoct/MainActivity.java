38226e62c2d638e510ec562133eadafb349f780b
2514817800@qq.com <2514817800@qq.com@927defb2-1d62-4751-8e14-6024133b83b5>
2021-05-18 03:47:43 +0000

    提交20210518修改

--- trunk/app/src/main/java/com/example/cnbiobasepoct/MainActivity.java
+++ trunk/app/src/main/java/com/example/cnbiobasepoct/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.cnbiobasepoct;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.v("提示","错误");
+
+        Button btnTest= (Button) findViewById(R.id.btnTest);
+        btnTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"测试",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
