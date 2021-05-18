9a50870d22554e11728fdbd0f829f8839bf95d16
2514817800 <2514817800@qq.com>
2021-05-18 03:47:43 +0000

    提交20210518修改

--- app/build/generated/data_binding_base_class_source_out/debug/out/com/example/cnbiobasepoct/databinding/ActivityMainBinding.java
+++ app/build/generated/data_binding_base_class_source_out/debug/out/com/example/cnbiobasepoct/databinding/ActivityMainBinding.java
@@ -4,6 +4,7 @@ package com.example.cnbiobasepoct.databinding;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -11,13 +12,18 @@ import androidx.viewbinding.ViewBinding;
 import com.example.cnbiobasepoct.R;
 import java.lang.NullPointerException;
 import java.lang.Override;
+import java.lang.String;
 
 public final class ActivityMainBinding implements ViewBinding {
   @NonNull
   private final ConstraintLayout rootView;
 
-  private ActivityMainBinding(@NonNull ConstraintLayout rootView) {
+  @NonNull
+  public final Button btnTest;
+
+  private ActivityMainBinding(@NonNull ConstraintLayout rootView, @NonNull Button btnTest) {
     this.rootView = rootView;
+    this.btnTest = btnTest;
   }
 
   @Override
@@ -43,10 +49,19 @@ public final class ActivityMainBinding implements ViewBinding {
 
   @NonNull
   public static ActivityMainBinding bind(@NonNull View rootView) {
-    if (rootView == null) {
-      throw new NullPointerException("rootView");
-    }
+    // The body of this method is generated in a way you would not otherwise write.
+    // This is done to optimize the compiled bytecode for size and performance.
+    int id;
+    missingId: {
+      id = R.id.btnTest;
+      Button btnTest = rootView.findViewById(id);
+      if (btnTest == null) {
+        break missingId;
+      }
 
-    return new ActivityMainBinding((ConstraintLayout) rootView);
+      return new ActivityMainBinding((ConstraintLayout) rootView, btnTest);
+    }
+    String missingId = rootView.getResources().getResourceName(id);
+    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
   }
 }
